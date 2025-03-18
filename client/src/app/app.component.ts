5c89e8c760d43ecad7d01879810cbf3c6898d24d
Stuart Reid <stuartreid81@gmail.com>
2025-03-18 06:54:22 +0000

    Added Angular client to the project

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'Warmaster';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get("https://localhost5001/apu/getusers").subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => { console.log('This request has completed') }
+    })
+  }
+}
